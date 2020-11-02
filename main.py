842e3990171e9b6152b3a533d9091f391dd477ec
Luis Combariza <luis_combariza@outlook.com>
2020-11-02 11:14:15 -0800

    Move and draw classes complete, still need to be tested

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # Luis Combariza  - November 2 / 2020
-
+# Calgary, AB
 
 ######################################################################
 ###################### AI LEANRS TO GAME #############################
 ######################################################################
+
+# The following game and AI implementation was created as a way to
+# to look into the possibilities that a simple neuro network has on
+# something so difficult to a human player yet so simple to an algorithm
 
 import pygame
 import neat
@@ -46,8 +50,45 @@ class Bird:
         self.height = self.y
 
     def move(self):
-        return
+        self.tick_count += 1
+        # movement logic of the game objectc
+        d = self.vel*self.tick_count + 1.5*self.tick_count**2
 
+        if d >= 16:
+            # Absolute velocity of object falling down
+            d = d/abs(d)* 16
+
+        if d < 0:
+            d -= 2
+
+        self.y = self.y + d
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > - 90:
+                self.tilt -= self.ROT_VEL
+
+
+    def draw(self,win):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.IMGS[1]
+            self.img_count = self.ANIMATION_TIME*2
 
         
 
