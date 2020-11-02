c1ab40c99f18cbd1d8197f2e584fa580e887cd2d
Luis Combariza <luis_combariza@outlook.com>
2020-11-02 13:27:19 -0800

    First copy of working game (without AI or human controls) complete. Need to implement human and IA mechanics for the game

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import os
 import random
 
 ## Size of the windows used as game
-WIN_WIDTH = 600
+WIN_WIDTH = 550
 WIN_HEIGHT = 800
 
 ## importing game graphics to be used
@@ -56,11 +56,49 @@ class Pipe:
         win.blit(self.PIPE_TOP,(self.x, self.top))
         win.blit(self.PIPE_BOTTOM,(self.x, self.bottom))
 
-    
+    # Collision method that checks if two images ( masks ) collide
+    def collide(self,bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        t_point = bird_mask.overlap(top_mask, top_offset)
+
+        if t_point or b_point:
+            return True
+        return False
+
+# class that represents the base of the game and how it will move
+class Base:
+    VEL = 5
+    WIDTH = BASE_IMG.get_width()
+    IMG = BASE_IMG
+
+    def __init__(self,y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.WIDTH
+
+    # Movement of the base
+    def move(self):
+        self.x1 -= self.VEL
+        self.x2 -= self.VEL
+
+        if self.x1 + self.WIDTH < 0:
+            self.x1 = self.x2 + self.WIDTH
+        if self.x2 + self.WIDTH < 0:
+            self.x2 = self.x1 + self.WIDTH
+
+    def draw(self,win):
+        win.blit(self.IMG, (self.x1,self.y))
+        win.blit(self.IMG, (self.x2, self.y))
 
 
-
-## bird class for the bird object
+# bird class for the bird object
 class Bird:
     IMGS = BIRD_IMGS
     MAX_ROTATION = 25
@@ -136,13 +174,21 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
-def draw_window(win,bird):
+def draw_window(win,bird,pipes,base):
     win.blit(BG_IMG, (0,0))
+    for pipe in pipes:
+        pipe.draw(win)
+    base.draw(win)
+
     bird.draw(win)
     pygame.display.update()
 
 def main():
-    bird = Bird(200,200)
+    bird = Bird(250,350)
+    base = Base(740)
+    pipes = [Pipe(700)]
+    score = 0
+
     win = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
 
     run = True
@@ -151,7 +197,27 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window(win,bird)
+        add_pipe = False
+        rem  = []
+        for pipe in pipes:
+            if pipe.collide(bird):
+                pass
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+            if not pipe.passed and pipe.x < bird.x:
+                pipe.passed = True
+                add_pipe = True
+
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            pipes.append(Pipe(800))
+        for r in rem:
+            pipes.remove(r)
+
+        base.move()
+        draw_window(win,bird,pipes,base)
     pygame.quit()
     quit()
 
